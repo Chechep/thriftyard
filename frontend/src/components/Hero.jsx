39def7fa5aa5ebe680bc611e3a8f7c2e1ba0724d
Brian Cheptiony <cheptiony6@gmail.com>
2025-10-10 14:00:24 +0300

    updates

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import welcomeImg from "../assets/welcome.png";
 
 export default function Hero() {
   return (
-    <section className="bg-gradient-to-r from-blue-900 to-blue-200 text-white dark:from-black dark:to-gray-700 py-28 md:py-32 transition-colors flex flex-col items-center justify-center text-center">
+    <section className="text-white py-28 md:py-32 transition-colors flex flex-col items-center justify-center text-center">
       {/* Animated Welcome Image */}
       <motion.img
         src={welcomeImg}
@@ -24,14 +24,15 @@ export default function Hero() {
 
       {/* Hero Text */}
       <div className="max-w-2xl">
-        <p className="text-lg mb-8 text-gray-100 dark:text-gray-300">
-          Your style journey starts here.
+        <p className="text-lg mb-8 text-black dark:text-white">
+          Discover amazing trendy fashion at unbeatable prices from our store.  <br />
+          Your style journey starts here
         </p>
         <Link
           to="/products"
           className="bg-yellow-400 dark:bg-yellow-500 text-gray-900 dark:text-gray-800 px-8 py-3 rounded-lg font-semibold shadow-md hover:bg-yellow-300 dark:hover:bg-yellow-400 transition-all"
         >
-          Shop with us
+          Shop Now
         </Link>
       </div>
     </section>
